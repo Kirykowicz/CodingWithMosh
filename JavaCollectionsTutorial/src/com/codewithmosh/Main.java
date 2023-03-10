511cdf3ba085e0abdadc2b4ab393c585e7a85485
Robert Kirykowicz <rakirykowicz@gmail.com>
2023-03-09 20:04:55 -0600

    examples of using the List interface

--- JavaCollectionsTutorial/src/com/codewithmosh/Main.java
+++ JavaCollectionsTutorial/src/com/codewithmosh/Main.java
@@ -1,10 +1,11 @@
 package com.codewithmosh;
 
 import com.codewithmosh.collections.CollectionsDemo;
+import com.codewithmosh.list.ListDemo;
 
 public class Main {
 
     public static void main(String[] args) {
-        CollectionsDemo.show();
+        ListDemo.show();
     }
 }
---
+++ JavaCollectionsTutorial/src/com/codewithmosh/list/ListDemo.java
@@ -0,0 +1,16 @@
+package com.codewithmosh.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo {
+
+    public static void show(){
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add(0, "!");
+        System.out.println(list);
+    }
+}
